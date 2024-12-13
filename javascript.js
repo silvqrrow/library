0ca52490cbb070322b77f409f070edd0045ff7d0
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-12-13 15:14:37 -0500

    Add display library function to display books from the library array

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ const myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.pages = pages + "pg.";
+  this.pages = pages;
   this.read = read;
 }
 
@@ -11,3 +11,41 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+
+const libraryContainer = document.querySelector(".book__container");
+
+function displayLibrary() {
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book__card");
+
+    /* Create elements for each part of the book */
+    const titleElement = document.createElement("p");
+    titleElement.textContent = `"${book.title}"`;
+    titleElement.classList.add("book__card-title");
+    bookCard.appendChild(titleElement);
+
+    const authorElement = document.createElement("p");
+    authorElement.textContent = `By: ${book.author}`;
+    authorElement.classList.add("book__card-author");
+    bookCard.appendChild(authorElement);
+
+    const pagesElement = document.createElement("p");
+    pagesElement.textContent = `${book.pages}pg.`;
+    pagesElement.classList.add("book__card-pages");
+    bookCard.appendChild(pagesElement);
+
+    const readElement = document.createElement("p");
+    readElement.textContent = `${book.read ? "Read" : "Not Read"}`;
+    readElement.classList.add("book__card-read");
+    bookCard.appendChild(readElement);
+
+    /* Add book to library container*/
+    libraryContainer.appendChild(bookCard);
+  });
+}
+
+/* TESTING */
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+displayLibrary();
