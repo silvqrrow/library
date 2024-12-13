65f1a93c89df3920954773d1d24f8d6538db50af
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-12-13 17:21:17 -0500

    Refractor codebase and add read status changer

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
+const libraryContainer = document.querySelector(".book__container");
+const bookDialog = document.querySelector(".dialog");
+const addBookButton = document.querySelector(".add-button");
+const addBookForm = document.querySelector(".add-book-form");
 
+/* Book Object*/
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,17 +12,19 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+/* Prototype read status */
+Book.prototype.readStatus = function () {
+  this.read = !this.read;
+  return this.read;
+};
+
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   displayLibrary();
 }
 
-const libraryContainer = document.querySelector(".book__container");
-const bookDialog = document.querySelector(".dialog");
-const addBookButton = document.querySelector(".add-button");
-const addBookForm = document.querySelector(".add-book-form");
-
+/* Form Handling */
 addBookForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const title = addBookForm.title.value;
@@ -33,6 +40,13 @@ addBookButton.addEventListener("click", function () {
   bookDialog.showModal();
 });
 
+/* Book Removal */
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayLibrary();
+}
+
+/* Main Display Function */
 function displayLibrary() {
   libraryContainer.replaceChildren();
 
@@ -43,7 +57,7 @@ function displayLibrary() {
 
     /* Create elements for each part of the book */
     const titleElement = document.createElement("p");
-    titleElement.textContent = `"${book.title}"`;
+    titleElement.textContent = `${book.title}`;
     titleElement.classList.add("book__card-title");
     bookCard.appendChild(titleElement);
 
@@ -57,11 +71,17 @@ function displayLibrary() {
     pagesElement.classList.add("book__card-pages");
     bookCard.appendChild(pagesElement);
 
-    const readElement = document.createElement("p");
+    const readElement = document.createElement("button");
     readElement.textContent = `${book.read ? "Read" : "Not Read"}`;
     readElement.classList.add("book__card-read");
     bookCard.appendChild(readElement);
+    /* Change status of book */
+    readElement.addEventListener("click", function () {
+      book.readStatus();
+      readElement.textContent = `${book.read ? "Read" : "Not Read"}`;
+    });
 
+    /* Remove book from library */
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.classList.add("remove");
@@ -73,9 +93,4 @@ function displayLibrary() {
     /* Add book to library container*/
     libraryContainer.appendChild(bookCard);
   });
-}
-
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  displayLibrary();
 }
