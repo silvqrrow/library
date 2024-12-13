1ebb5a9b1d904f6ae1de6793bd8dac3ded728f43
silvqrrow <105092230+silvqrrow@users.noreply.github.com>
2024-12-13 13:20:38 -0500

    Add function to create book and a function to store it in the library

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
